740e3787c14066e97f9cc069ea29e5e4871917ec
Renat Gubaev <gubaev.r@n-t.io>
2024-08-02 16:24:47 +0300

    auto commit

--- src/public-api.ts
+++ src/public-api.ts
@@ -27,3 +27,5 @@ export * from './database/entity/EntityManager';
 export * from './database/entity/EntityManagerImpl';
 export * from './database/entity/EntityRelation';
 export * from './database/entity/EntityRelationChild';
+//
+export * from './transport/HlfTransportCommand';
